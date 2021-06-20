2208beaa2f30927c7222217e81fb5ee846dc0473
Nikita Denisov <nikitadenisovvalerevich@gmail.com>
2021-06-21 00:45:17 +0500

    Implement deleting the book

--- app/src/main/java/com/ham/library/books_view/BookInfoActivity.java
+++ app/src/main/java/com/ham/library/books_view/BookInfoActivity.java
@@ -1,26 +1,140 @@
 package com.ham.library.books_view;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.ham.library.BaseActivity;
 import com.ham.library.R;
+import com.ham.library.dao.entity.BookEntity;
+import com.ham.library.dao.entity.ReviewEntity;
+import com.ham.library.dao.model.Book;
+import com.ham.library.dao.model.Review;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
 
 public class BookInfoActivity extends BaseActivity {
+    private ItemAdapter adapter;
+    private BookEntity bookEntity;
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_info);
+        this.adapter = new ItemAdapter();
+
+        Intent this_intent = getIntent();
+
+        String author_string = this_intent.getStringExtra("author");
+        String title_string = this_intent.getStringExtra("title");
+        double rating = this_intent.getDoubleExtra("rating", 0);
+        Integer id = this_intent.getIntExtra("id", 0);
+
 
         TextView info_author = findViewById(R.id.info_author);
-        info_author.setText(getIntent().getStringExtra("author"));
+        info_author.setText(author_string);
 
         TextView info_title = findViewById(R.id.info_title);
-        info_title.setText(getIntent().getStringExtra("title"));
+        info_title.setText(title_string);
 
         TextView info_rating = findViewById(R.id.info_rating);
-        info_title.setText(getIntent().getIntExtra("rating", 0));
+        info_rating.setText("Рейтинг:" + rating);
+        bookEntity = new BookEntity();
+        bookEntity.id = id;
+        bookEntity.author = author_string;
+        bookEntity.rating = rating;
+        bookEntity.title = title_string;
 
+        Button deleteBookButton = findViewById(R.id.delete_book_button);
+        deleteBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteBook();
+            }
+        });
+
+        mainViewModel.getBookReviews(id).observe(this, new Observer<List<ReviewEntity>>() {
+            @Override
+            public void onChanged(List<ReviewEntity> reviewEntities) {
+                fetchData(reviewEntities);
+            }
+        });
+    }
+
+    private void deleteBook(){
+        Executors.newSingleThreadExecutor().execute(() -> mainViewModel.deleteBook(this.bookEntity));
+        finish();
+    }
+
+    public void fetchData(List<ReviewEntity> reviewEntities){
+        ArrayList<Review> reviewsFromBD = new ArrayList<>();
+        for (ReviewEntity listEntity : reviewEntities) {
+            //string to not repeat extracting Review class with Review entities
+            com.ham.library.dao.entity.Review review = listEntity.reviewEntity;
+            reviewsFromBD.add(new Review(review.id, review.text, review.bookID, review.rating, review.timestamp));
+        }
+        this.adapter.setDataList(reviewsFromBD);
+    }
+
+    public final static class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+        private ArrayList<Review> dataList = new ArrayList<>();
+
+        public static class ViewHolder extends RecyclerView.ViewHolder{
+            private TextView timestamp;
+            private TextView review;
+            private TextView rating;
+            public ViewHolder(View itemView){
+                super(itemView);
+                this.timestamp = itemView.findViewById(R.id.timestamp);
+                this.review = itemView.findViewById(R.id.review_text);
+                this.rating = itemView.findViewById(R.id.rating_review);
+            }
+
+            public void bind(final Review data){
+                this.timestamp.setText(data.getTimestamp().toString());
+                this.review.setText(data.getText());
+                this.rating.setText("Рейтинг:" + data.getRating());
+            }
+        }
+
+        public void setDataList(ArrayList<Review> dataList) {
+            this.dataList = new ArrayList<>();
+            if (dataList != null) {
+                this.dataList.addAll(dataList);
+            }
+            notifyDataSetChanged();
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            Context context = parent.getContext();
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View contactView = inflater.inflate(R.layout.book_item_layout, parent, false);
+            return new BookInfoActivity.ItemAdapter.ViewHolder(contactView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull @NotNull RecyclerView.ViewHolder holder, int position) {
+            Review data = this.dataList.get(position);
+            ((BookInfoActivity.ItemAdapter.ViewHolder) holder).bind(data);
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataList.size();
+        }
     }
 }
